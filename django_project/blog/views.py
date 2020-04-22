66a815bab35a5f5e79540df9afb0ff8ffa81a5bb
gargnandini <nandinigarh1204@gmail.com>
2020-04-23 04:41:47 +0530

    project under work

---
+++ django_project/blog/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'CoreysMS',
+        'title': 'Blog Post 1',
+        'content':'First post content',
+        'date_posted':'April 22, 2020'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content':'Second post content',
+        'date_posted':'April 22, 2020'
+    }
+]
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context) 
+
+def about(request):
+        return render(request, 'blog/about.html', {'title': 'About'}) 
+
